d99a6373ce5a3d70f83a90141611dad37fba8a76
Dan Ahern <danahern@gmail.com>
2010-11-15 12:40:52 -0500

    Gem file for monetra

---
+++ pkg/monetra-0.0.2/lib/monetra/configuration.rb
@@ -0,0 +1,32 @@
+module Monetra
+	class Configuration
+		class << self
+			def options(options_or_path_to=nil)
+				@options ||= case options_or_path_to.class.to_s
+				when "String"
+					YAML::load_file(options_or_path_to)
+				when "Hash"
+					options_or_path_to
+				when "NilClass"
+					YAML::load_file(File.join(Rails.root, 'config', 'monetra.yml'))[Rails.env]
+				end 
+			end
+		
+			def user_name
+				options[:user_name]
+			end
+		
+			def password
+				options[:password]
+			end
+		
+			def host
+				options[:host]
+			end
+		
+			def port
+				options[:port]
+			end
+		end
+	end
+end
