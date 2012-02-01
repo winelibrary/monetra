60c3df260ed9671fef73eabe8b6bed82ac797e87
Dan Ahern <danahern@gmail.com>
2012-02-01 12:37:54 -0500

    Latest monetra configuration to support more ssl

--- lib/monetra/configuration.rb
+++ lib/monetra/configuration.rb
@@ -4,52 +4,52 @@ module Monetra
       def options(options_or_path_to=nil)
         @options ||= case options_or_path_to.class.to_s
         when "String"
-          YAML::load_file(options_or_path_to).stringify_keys
+          YAML::load_file(options_or_path_to).symbolize_keys
         when "Hash"
-          options_or_path_to.stringify_keys
+          options_or_path_to.symbolize_keys
         when "NilClass"
-          YAML::load_file(File.join(Rails.root, 'config', 'monetra.yml'))[Rails.env].stringify_keys if defined?(Rails)
+          YAML::load_file(File.join(Rails.root, 'config', 'monetra.yml'))[Rails.env].symbolize_keys if defined?(Rails)
         end
       end
 
       def user_name
-        Monetra::Configuration.options["user_name"]
+        Monetra::Configuration.options[:user_name]
       end
 
       def user_name=(value)
-        Monetra::Configuration.options["user_name"] = value
+        Monetra::Configuration.options[:user_name] = value
       end
 
       def password
-        Monetra::Configuration.options["password"]
+        Monetra::Configuration.options[:password]
       end
 
       def password=(value)
-        Monetra::Configuration.options["password"] = value
+        Monetra::Configuration.options[:password] = value
       end
 
       def host
-        Monetra::Configuration.options["host"]
+        Monetra::Configuration.options[:host]
       end
 
       def host=(value)
-        Monetra::Configuration.options["host"] = value
+        Monetra::Configuration.options[:host] = value
       end
 
       def port
-        Monetra::Configuration.options["port"]
+        Monetra::Configuration.options[:port]
       end
 
       def port=(value)
-        Monetra::Configuration.options["port"] = value
+        Monetra::Configuration.options[:port] = value
       end
 
       def use_ssl?
-        Monetra::Configuration.options["use_ssl"]
+        Monetra::Configuration.options[:use_ssl]
       end
 
       def use_ssl=(value)
-        Monetra::Configuration.options["use_ssl"] = value
+        Monetra::Configuration.options[:use_ssl] = value
       end
     end
   end
--- lib/monetra/connection.rb
+++ lib/monetra/connection.rb
@@ -7,16 +7,26 @@ module Monetra
 
       def post(data)
         base.use_ssl = Monetra::Configuration.use_ssl?
-        if Monetra::Configuration.use_ssl?
-          if RUBY_VERSION.to_f == 1.9
-            # base.verify_mode = OpenSSL::SSL::VERIFY_PEER
-            base.ca_file = '/usr/lib/ssl/certs/ca-certificates.crt' if File.exists?('/usr/lib/ssl/certs/ca-certificates.crt') # Ubuntu
-            base.ca_path = '/etc/ssl/certs' if File.exists?('/etc/ssl/certs') # Ubuntu
-            base.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt' if File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X
-          end
-        end
+        ssl_certificate if Monetra::Configuration.use_ssl?
         request = base.post("/", data)
         request.body
+      end
+
+      private
+      def ssl_certificate
+        if RUBY_VERSION.to_f == 1.9
+          # base.verify_mode = OpenSSL::SSL::VERIFY_PEER
+          case
+          when ENV["SSL_CERT_FILE"]
+            base.ca_file = ENV["SSL_CERT_FILE"]
+          when File.exists?('/usr/lib/ssl/certs/ca-certificates.crt') # heroku
+            base.ca_file = '/usr/lib/ssl/certs/ca-certificates.crt'  
+          when File.exists?('/etc/ssl/certs') # Ubuntu
+            base.ca_path = '/etc/ssl/certs'
+          when File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X
+            base.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt'
+          end
+        end
       end
     end
   end
